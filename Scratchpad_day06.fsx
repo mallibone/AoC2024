60d46219be7ca5b7d16a27feff7198fe76678cde
Mark Allibone <mark.allibone@rey-technology.com>
2024-12-06 22:16:23 +0100

    Solve day 6 part II - not super efficiently

--- Scratchpad_day06.fsx
+++ Scratchpad_day06.fsx
@@ -38,8 +38,7 @@ type Direction =
     | Left
     | Right
 
-
-type GameState = {GuardPosition: int * int; GuardDirection:Direction; Map: string array array}
+type GameState = {GuardPosition: int * int; GuardDirection:Direction; Map: string array array; VisitedPositions : ((int * int) * Direction) Set; IsLoop: bool}
 
 let rec findCoordinatePosition (map: string array array) character =
     map 
@@ -50,17 +49,27 @@ let rec findCoordinatePosition (map: string array array) character =
 let parseInput (inputLine: string array) =
     let map = inputLine |> Array.map (fun x -> x.ToCharArray() |> Array.map string)
     let guardPosition =  findCoordinatePosition map "^" |> Array.head
-    map[guardPosition |> snd].[guardPosition |> fst] <- "X"
-    {GuardPosition = guardPosition; GuardDirection = Up; Map = map}
+    // map[guardPosition |> snd].[guardPosition |> fst] <- "X"
+    {GuardPosition = guardPosition; GuardDirection = Up; Map = map; VisitedPositions = [(guardPosition, Up)] |> Set.ofList; IsLoop = false}
 
-let rec moveGuard (gameState: GameState) =
-    let (x, y) = gameState.GuardPosition
-    let newGuardPosition = 
+let changeDirection currentDirection =
+    match currentDirection with
+    | Up -> Right
+    | Right -> Down
+    | Down -> Left
+    | Left -> Up
+
+
+let getNextGuardPositionx (gameState: GameState) =
+        let (x, y) = gameState.GuardPosition
         match gameState.GuardDirection with
         | Up -> (x, y - 1)
         | Down -> (x, y + 1)
         | Left -> (x - 1, y)
         | Right -> (x + 1, y)
+
+let rec moveGuard (gameState: GameState) =
+    let newGuardPosition = getNextGuardPositionx gameState
 
     let (newX, newY) = newGuardPosition
     let maxY = gameState.Map.Length - 1
@@ -68,31 +77,42 @@ let rec moveGuard (gameState: GameState) =
 
     if newX < 0 || newY < 0 || newX > maxX || newY > maxY then
         gameState
+    elif gameState.VisitedPositions |> Seq.exists (fun (position, direction) -> position = newGuardPosition && direction = gameState.GuardDirection) then
+        { gameState with IsLoop = true }
     else
         let cell = gameState.Map.[newY].[newX]
         if cell = "#" then
-            let newDirection =
-                match gameState.GuardDirection with
-                | Up -> Right
-                | Right -> Down
-                | Down -> Left
-                | Left -> Up
+            let newDirection = changeDirection gameState.GuardDirection
             moveGuard { gameState with GuardDirection = newDirection }
         else
             let newMap = Array.copy gameState.Map
-            newMap.[newY].[newX] <- "X"
-            moveGuard { gameState with GuardPosition = newGuardPosition; Map = newMap }
+            moveGuard { gameState with GuardPosition = newGuardPosition; Map = newMap; VisitedPositions = gameState.VisitedPositions.Add(newGuardPosition, gameState.GuardDirection) }
 
 // part 1
 getInput 6
 // getTestInput 6
 |> parseInput
 |> moveGuard
-|> fun gs -> gs.Map
-|> Array.collect (fun m -> m |> Array.filter(fun x -> x = "X"))
-|> Array.length
+|> fun gs -> gs.VisitedPositions
+|> Seq.map (fun (x, _) -> x) |> Set.ofSeq
+|> Seq.length
 
 // part 2
-// getInput 6
+let findGuardLoops (gameState:GameState) =
+    let initialGuardPos = findCoordinatePosition gameState.Map "^" |> Array.head
+    gameState.VisitedPositions |> Seq.tail |> Seq.map (fun (pos, _) -> pos) |> Set.ofSeq
+    |> Set.map (fun (x,y) -> 
+        let map' = gameState.Map |> Array.map Array.copy
+        map'[y][x] <- "#"
+        {GuardPosition = initialGuardPos; GuardDirection = Up; Map = map'; VisitedPositions = [initialGuardPos, Up] |> Set.ofList; IsLoop = false})
+    |> PSeq.map moveGuard
+    |> PSeq.filter (fun gs -> gs.IsLoop)
+    |> PSeq.toList
+
+getInput 6
 // getTestInput 6
+|> parseInput
+|> moveGuard
+|> findGuardLoops
+|> List.length
 

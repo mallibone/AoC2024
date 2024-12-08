dd33af9faab3e73155013ef9ef467c845df759c6
Mark Allibone <mark.allibone@rey-technology.com>
2024-12-08 10:09:32 +0100

    Day 8 part II

--- Scratchpad_day08.fsx
+++ Scratchpad_day08.fsx
@@ -1,36 +1,16 @@
 #time
 #r "nuget: FSharp.Collections.ParallelSeq, 1.2.0"
 open System.IO
-open System.Text.RegularExpressions
 open System
-open System.Linq
 open System.Collections.Generic
-open FSharp.Collections.ParallelSeq
-open System.Collections.Concurrent
 
 let getTestInput (day:int) =
     let filename = Path.Combine(__SOURCE_DIRECTORY__, $"Input/TestDay{day:D2}.txt")
     File.ReadAllLines(filename)
-    // File.ReadAllText(filename)
 
 let getInput (day:int) =
     let filename = Path.Combine(__SOURCE_DIRECTORY__, $"Input/Day{day:D2}.txt")
     File.ReadAllLines(filename)
-    // File.ReadAllText(filename)
-
-
-// let (|Match|_|) (pat: string) (inp: string) =
-//     let m = Regex.Match(inp, pat) in
-
-//     if m.Success then
-//         Some(List.tail [ for g in m.Groups -> g.Value ])
-//     else
-//         None
-
-// let parseInput inputLine =
-//     inputLine |> function
-//         | Match "Blueprint (.*): Each ore robot costs (.*) ore. Each clay robot costs (.*) ore. Each obsidian robot costs (.*) ore and (.*) clay. Each geode robot costs (.*) ore and (.*) obsidian." [ blueprintId; oreRobotCost; clayRobotCost; obsidianRobotOreCost; obsidianRobotClayCost; geodeRobotOreCost; geodeRobotObsidianCost; ] ->
-//             {Id = int blueprintId; OreRobotOreCost = int oreRobotCost; ClayRobotOreCost = int clayRobotCost; ObsidianRobotOreCost = int obsidianRobotOreCost; ObsidianRobotClayCost = int obsidianRobotClayCost; GeodeRobotObsidianCost = int geodeRobotObsidianCost; GeodeRobotOreCost = int geodeRobotOreCost}
 
 let parseInput (input:string array) =
     let map = input |> Array.map (fun x -> x.ToCharArray() |> Array.map string)
@@ -45,9 +25,11 @@ let parseInput (input:string array) =
     )
     antennaCoords, map
 
-let findAntiNodes (antennaCoords:Dictionary<string, Tuple<int,int> list>, map:string array array) =
+let findAntiNodes antiNodeFunc (antennaCoords:Dictionary<string, Tuple<int,int> list>, map:string array array) =
     let maxY = map.Length
     let maxX = map.[0].Length
+
+    let initAnitNodeFunc = antiNodeFunc maxX maxY
 
     let antiNodes = 
         antennaCoords.Values
@@ -56,8 +38,8 @@ let findAntiNodes (antennaCoords:Dictionary<string, Tuple<int,int> list>, map:st
             |> List.collect (fun (x, y) -> 
                 coords 
                 |> List.map (fun (x2,y2) -> x2-x, y2-y) 
-                |> List.filter (fun (x,y) -> x <> 0 && y <> 0)
-                |> List.map (fun (dx,dy) -> x-dx, y-dy)
+                |> List.filter (fun (dx,dy) -> dx <> 0 && dy <> 0)
+                |> List.collect (fun (dx,dy) -> initAnitNodeFunc x y dx dy )
                 |> List.filter (fun (x,y) -> x >= 0 && y >= 0 && x < maxX && y < maxY)
             )
             |> List.distinct
@@ -66,23 +48,22 @@ let findAntiNodes (antennaCoords:Dictionary<string, Tuple<int,int> list>, map:st
 
     antiNodes
 
-let drawMap ((map:string array array), (coords:(int*int) list)) =
-    coords |> List.iter (fun (x,y) -> map[y][x] <- "#")
-    map
-
 // part 1
 getInput 8
 // getTestInput 8
 |> parseInput
-|> findAntiNodes
-// |> drawMap
+|> findAntiNodes (fun _ _ x y dx dy -> [x-dx, y-dy])
 |> Seq.length 
 
-
-
-
-
 // part 2
-// getInput 8
-// getTestInput 8
+let resonantHarmonicAntiNodes (maxX:int) (maxY:int) (x:int) (y:int) (dx:int) (dy:int) =
+    let rec loop (x:int) (y:int) antiNodeCoords =
+        if x < 0 || y < 0 || x >= maxX || y >= maxY then antiNodeCoords
+        else loop (x-dx) (y-dy) ((x,y)::antiNodeCoords)
+    loop x y []
 
+getInput 8
+// getTestInput 8
+|> parseInput
+|> findAntiNodes resonantHarmonicAntiNodes
+|> Seq.length
